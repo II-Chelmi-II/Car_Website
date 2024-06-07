438c6839590582cc28270d1e9b1505f9631c565d
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-06-07 23:52:53 +0300

    chore: Add RapidAPI

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { Fragment, SetStateAction, useState } from "react";
-import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { Combobox, Transition } from "@headlessui/react";
 
 import { manufacturers } from "@/constants";
-import { SearchManuFacturerProps } from "@types";
+import { SearchManuFacturerProps } from "@/types";
 
 const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacturerProps) => {
   const [query, setQuery] = useState("");
@@ -22,7 +22,8 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
     <div className='search-manufacturer'>
       <Combobox value={manufacturer} onChange={setManuFacturer}>
         <div className='relative w-full'>
-          <ComboboxButton className='absolute top-[14px]'>
+          {/* Button for the combobox. Click on the icon to see the complete dropdown */}
+          <Combobox.Button className='absolute top-[14px]'>
             <Image
               src='/car-logo.svg'
               width={20}
@@ -30,60 +31,63 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
               className='ml-4'
               alt='car logo'
             />
-          </ComboboxButton>
+          </Combobox.Button>
 
-          <ComboboxInput
+          {/* Input field for searching */}
+          <Combobox.Input
             className='search-manufacturer__input'
             displayValue={(item: string) => item}
-            onChange={(event) => setQuery(event.target.value)}
+            onChange={(event) => setQuery(event.target.value)} // Update the search query when the input changes
             placeholder='Volkswagen...'
           />
 
+          {/* Transition for displaying the options */}
           <Transition
-            as={Fragment} 
+            as={Fragment} // group multiple elements without introducing an additional DOM node i.e., <></>
             leave='transition ease-in duration-100'
             leaveFrom='opacity-100'
             leaveTo='opacity-0'
-            afterLeave={() => setQuery("")} 
+            afterLeave={() => setQuery("")} // Reset the search query after the transition completes
           >
-            <ComboboxOptions
+            <Combobox.Options
               className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
               static
             >
               {filteredManufacturers.length === 0 && query !== "" ? (
-                <ComboboxOption
+                <Combobox.Option
                   value={query}
                   className='search-manufacturer__option'
                 >
                   Create "{query}"
-                </ComboboxOption>
+                </Combobox.Option>
               ) : (
                 filteredManufacturers.map((item) => (
-                  <ComboboxOption
+                  <Combobox.Option
                     key={item}
-                    className={({ focus }) =>
+                    className={({ active }) =>
                       `relative search-manufacturer__option ${
-                        focus ? "bg-primary-blue text-white" : "text-gray-900"
+                        active ? "bg-primary-blue text-white" : "text-gray-900"
                       }`
                     }
                     value={item}
                   >
-                    {({ selected, focus }) => (
+                    {({ selected, active }) => (
                       <>
                         <span className={`block truncate ${selected ? "font-medium" : "font-normal"}`}>
                           {item}
                         </span>
 
+                        {/* Show an active blue background color if the option is selected */}
                         {selected ? (
-                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${focus? "text-white": "text-pribg-primary-purple"}`}
+                          <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active? "text-white": "text-pribg-primary-purple"}`}
                           ></span>
                         ) : null}
                       </>
                     )}
-                  </ComboboxOption>
+                  </Combobox.Option>
                 ))
               )}
-            </ComboboxOptions>
+            </Combobox.Options>
           </Transition>
         </div>
       </Combobox>
