72f48faaf183e4e49fb8594f4648696e96621218
Maharavo1 <hei.maharavo@gmail.com>
2024-06-13 16:36:49 +0300

    fix: resolve bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,42 +1,42 @@
-"use client"
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 
 import CustomButton from "./CustomButton";
-import { useReducer } from "react";
 import { useRouter } from "next/navigation";
 
 const Navbar = () => {
     const router = useRouter();
-    const handleLoginSignupClick = () => {
-      router.push("/login");
+
+    const handleSignInClick = () => {
+        router.push("/login");
     };
 
-  return (
-    <header className="w-full absolute z-10">
-        <nav className="max-w-[1440px] mx-auto 
-        flex justify-between items-center 
-        sm:px-16 px-6 py-4">
-            <Link href="/" className="flex justify-center items-center">
-                <Image 
-                    src="/logo.svg"
-                    alt="Car Hub Logo"
-                    width={118}
-                    height={18}
-                    className="object-contain"
-                />
-            </Link>
+    return (
+        <header className="w-full absolute z-10">
+            <nav className="max-w-[1440px] mx-auto 
+            flex justify-between items-center 
+            sm:px-16 px-6 py-4">
+                <Link href="/" className="flex justify-center items-center">
+                    <Image 
+                        src="/logo.svg"
+                        alt="Car Hub Logo"
+                        width={118}
+                        height={18}
+                        className="object-contain"
+                    />
+                </Link>
 
-            <CustomButton 
-                title="Sign in"
-                btnType="button"
-                containerStyles="text-primary-blue
-                rounded-full bg-white min-w-[130px]"
-                onClick={router.push("/login")}
-            />
-        </nav>
-    </header>
-  )
+                <CustomButton 
+                    title="Sign in"
+                    btnType="button"
+                    containerStyles="text-primary-blue
+                    rounded-full bg-white min-w-[130px]"
+                    handleClick={handleSignInClick}
+                />
+            </nav>
+        </header>
+    )
 }
 
-export default Navbar
+export default Navbar;
